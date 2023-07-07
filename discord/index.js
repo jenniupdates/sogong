b1b9e94c88cef4c95891049b71a5c5480e8354cb
Yin Shan, Lim <ysimptstuff@gmail.com>
2023-07-08 02:34:23 +0800

    for ristonia sucks channel

--- discord/index.js
+++ discord/index.js
@@ -27,17 +27,18 @@ client.on('interactionCreate', (interaction) => {
 
     if (interaction.commandName === "start") {
         interaction.reply("Starting game...\n- 🟢 means that the number is in the right place \n- ⚠️ means that the number exists but in the wrong place \n- ❌ means that the number does not exist at all \n Use command \`/input <number>\` to test a number");
-        dict[interaction.user.id] = Mastermind.generateRandomNumber();
-        console.log("Generated number: ", dict[interaction.user.id]);
+        dict[interaction.user.id] = [Mastermind.generateRandomNumber(),0];
+        console.log("Generated number: ", dict[interaction.user.id][0]);
     }
 
     if (interaction.commandName === "input") {
         if (interaction.user.id in dict) {
             let userInput = interaction.options.get('input-number');
+            dict[interaction.user.id][1] += 1
             console.log(`Generated number: ${dict[interaction.user.id]}, User input: ${userInput.value}`)
             let result = Mastermind.checkAnswer(userInput.value, dict[interaction.user.id]);
             if (result["🟢"] == 3) {
-                interaction.reply(`Your input is: ${userInput.value}\n ${JSON.stringify(result)}\n YOU WIN, GAME ENDS. YOU CAN START ANOTHER VIA COMMAND \`/start\``);
+                interaction.reply(`Turn ${dict[interaction.user.id][1]}: Your input is: ${userInput.value}\n ${JSON.stringify(result)}\n YOU WIN, GAME ENDS. YOU CAN START ANOTHER VIA COMMAND \`/start\``);
                 dict = {};
             }
             else {
@@ -50,19 +51,20 @@ client.on('interactionCreate', (interaction) => {
     }
 })
 
-// client.on("messageCreate", async (message) => {
-//     console.log("Content: ", message.content);
-//     let msg = {};
-//     msg.id = message.id;
-//     msg.content = message.content;
-//     msg.creator = message.author.id;
-//     console.log(msg);
+client.on("messageCreate", async (message) => {
+    console.log("Content: ", message.content);
+    let msg = {};
+    msg.id = message.id;
+    msg.guildid = message.guildId;
+    msg.content = message.content;
+    msg.creator = message.author.id;
+    console.log(msg);
 
-//     if (!message?.author.bot) {
-//         // message.author.send(`Echo ${msg.content}`);
-//         message.reply(`Echo ${msg.content}`)
-//     }
-// }); 
+    if (!message?.author.bot) {
+        // message.author.send(`Echo ${msg.content}`);
+        message.reply(`Echo ${msg.content}`)
+    }
+}); 
 
 // Create a slash command builder
 // const startGame = new SlashCommandBuilder()
